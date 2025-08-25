dbbad2822caa5f6254932e208e1b32791a91860b
Matas Backevicius <84940755+Mugdass@users.noreply.github.com>
2025-08-25 11:32:06 -0700

    Update DBConnection.java

--- src/test/java/dbvalidation/DBConnection.java
+++ src/test/java/dbvalidation/DBConnection.java
@@ -1,1 +1,15 @@
+package dbvalidation;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/testdb";
+        String user = "testuser";
+        String password = "testpass";
+        return DriverManager.getConnection(url, user, password);
+    }
+}
 
