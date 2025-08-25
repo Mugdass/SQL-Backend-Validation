b3b74fc2c6edb4662fbbee1c60adb81129cb7e5c
Matas Backevicius <84940755+Mugdass@users.noreply.github.com>
2025-08-25 11:33:15 -0700

    Update DBValidationTests.java

--- src/test/java/dbvalidation/DBValidationTests.java
+++ src/test/java/dbvalidation/DBValidationTests.java
@@ -1,1 +1,24 @@
+package dbvalidation;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.sql.*;
+
+public class DBValidationTests {
+
+    @Test
+    public void testUserCount() throws SQLException {
+        Connection conn = DBConnection.getConnection();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM users");
+
+        rs.next();
+        int count = rs.getInt(1);
+
+        Assert.assertTrue(count > 0, "User count should be greater than 0");
+
+        conn.close();
+    }
+}
 
